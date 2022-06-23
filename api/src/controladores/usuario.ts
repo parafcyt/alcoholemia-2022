d3c86e7faefcf9cf7c5c72b655679f0d882cec25
Beatriz Vazquez <45252114+parafcyt@users.noreply.github.com>
2022-06-23 17:19:16 -0300

    agrego alta de usuario

--- api/src/controladores/usuario.ts
+++ api/src/controladores/usuario.ts
@@ -58,3 +58,23 @@ export async function UpdateUsuario(req: Request, res: Response){
         res.status(500).send(error);
     }
 }
+
+export async function AddUsuario(req:Request,res:Response){
+    try {
+        const clienteBbdd: Pool= bbddCliente();
+        const consulta :string='select * from altaUsuario($1,$2,$3,$4)';
+        const usuarioParam:string[]=[
+            req.body.nombreUsuario,
+            req.body.contrasenia,
+            req.body.nombreReal,
+            req.body.tipoUsuarioId
+        ];
+
+        const respuesta:QueryResult=await clienteBbdd.query(consulta,usuarioParam);
+        res.status(200).send(respuesta.rows);
+    }
+    catch (error){
+        res.status(500).send(error);
+    }
+
+}
--- api/src/rutas/usuarios.ts
+++ api/src/rutas/usuarios.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";   
 
-import { GetUsuarios, DeleteUsuario, UpdateUsuario } from "../controladores/usuario";
+import { GetUsuarios, DeleteUsuario, UpdateUsuario, AddUsuario } from "../controladores/usuario";
 
 const router : Router= Router();
 
 router.get('/usuario/todos', GetUsuarios);
 router.delete('/usuario/:nombreUsuario', DeleteUsuario);
 router.put('/usuario', UpdateUsuario);
+router.post('/usuario/nuevo',AddUsuario);
 
 
 export default router;
